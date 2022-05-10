524d4b0fc66de271beda0185ccca9fd19b3bb762
Lucas Desenvolvedor <LUCAS_ARAGAO_FEL@HOTMAIL.COM>
2022-05-09 21:11:10 -0300

    segunda parte da aula

---
+++ Backend e serviços na nuvem da Microsoft Azure/aula03/EJOGOS/Controllers/EquipeController.cs
@@ -0,0 +1,28 @@
+﻿using EJOGOS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EJOGOS.Controllers
+{
+    public class EquipeController : Controller
+    {
+
+        //ActionResult representam os varios codigos HTTP,
+
+        //Codigos HTTP - https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status
+
+
+        Equipe equipeModel = new Equipe();
+
+        public IActionResult Index()
+        {
+            //ViewBag = Reserva de espaco para armazenar informacoes para recuparar na VIEW.
+
+            //ViewBag = tem a função de "carregar" as informaçoes do Controller para a 
+             // VIEW.
+
+            ViewBag.Equipes = equipeModel.LerEquipes();
+
+            return View();
+        }
+    }
+}
--- Backend e serviços na nuvem da Microsoft Azure/aula03/EJOGOS/Models/Equipe.cs
+++ Backend e serviços na nuvem da Microsoft Azure/aula03/EJOGOS/Models/Equipe.cs
@@ -1,5 +1,6 @@
 ﻿using EJOGOS.Interface;
 using System.Collections.Generic;
+using System.IO;
 
 namespace EJOGOS.Models
 {
@@ -18,7 +19,7 @@ namespace EJOGOS.Models
 
 
         public Equipe()
-        {          
+        {
 
             //metodo da classe de herança EquipeBase.
             CriarPastaOuArquivo(caminhobd);
@@ -26,28 +27,48 @@ namespace EJOGOS.Models
 
 
         //criar uma funcao que vai receber o objeto EQUIPE,
-         // e vai retornar no formato de csv. (STRING)
+        // e vai retornar no formato de csv. (STRING)
 
         private string Preparar(Equipe e)
         {
             //return e.idEquipe + ";" + e.Nome + ";" + e.Imagem;
 
-             return $"{ e.idEquipe};{e.Nome};{e.Imagem}";
+            return $"{ e.idEquipe};{e.Nome};{e.Imagem}";
 
         }
-
-
-
-
-
         public void Criar(Equipe novaequipe)
         {
-            throw new System.NotImplementedException();
+            string valor = Preparar(novaequipe);
+
+            //array de strings =   na posicao 0 temos o retorno do "Preparar".
+            string[] equipe_texto = { valor };
+
+            //arquivo vamos adicionar uma nova linha.
+            //caminho do arquivo,  string em formato array.
+            File.AppendAllLines(caminhobd, equipe_texto);
         }
 
         public List<Equipe> LerEquipes()
         {
-            throw new System.NotImplementedException();
+            List<Equipe> listaEquipes = new List<Equipe>();
+
+            //ler todas as linhas do arquivo csv, armazenar em um array;
+            string[] linhas = File.ReadAllLines(caminhobd);
+
+            foreach (string item in linhas)
+            {
+                // 1; nome da equipe;  caminho da imagem
+                string[] linhaEquipe = item.Split(';');
+
+                Equipe equipeAtual = new Equipe();
+                equipeAtual.idEquipe = int.Parse(linhaEquipe[0]);
+                equipeAtual.Nome = linhaEquipe[1];
+                equipeAtual.Imagem = linhaEquipe[2];
+
+                listaEquipes.Add(equipeAtual);
+            }
+            return listaEquipes;
+
         }
     }
 }
